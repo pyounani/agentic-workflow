7dc2de7da2b4ff188efd5692777b30951afd26e5
pyounani <yndbsk9372@gmail.com>
2026-06-05 16:09:43 +0900

    feat: add LanternDetailResponse schema (#4)

--- app/schemas/lantern.py
+++ app/schemas/lantern.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
 
 from app.enums import LanternStatus
@@ -7,3 +10,12 @@ class LanternCreateResponse(BaseModel):
     lantern_code: str
     name: str
     status: LanternStatus
+
+
+class LanternDetailResponse(BaseModel):
+    lantern_code: str
+    name: str
+    image_paths: list[str]
+    background_music: Optional[str]
+    status: LanternStatus
+    created_at: datetime
